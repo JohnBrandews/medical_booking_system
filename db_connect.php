7b2c7cdfd717cf3d955f74ea02fc746516539fbe
john Brandews <johnbrandews@gmail.com>
2024-12-02 01:12:33 +0300

    added php file  that contain the database connectivity

---
+++ db_connect.php
@@ -0,0 +1,15 @@
+
+<?php
+
+$host = 'localhost';
+$dbname = 'medenin_clinic';
+$username = 'root';
+$password = ''; // Default XAMPP password
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+?>
